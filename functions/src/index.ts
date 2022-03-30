43f4995cf9d5c2bb071b4fe7b5487b4fdc85eb99
Evan Lihou <e@evandoes.dev>
2022-03-30 13:06:17 -0400

    Oops. Functions are hard

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 const {updateCurrentMatch} = require("./updateCurrentMatch");
 
 admin.initializeApp();
