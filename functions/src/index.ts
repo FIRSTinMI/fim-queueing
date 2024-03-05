de4f85b5dba04eae96121585f318e338fa25f9f9
evanlihou <evan@evanlihou.com>
2024-03-05 15:40:10 -0500

    Add a listener for updates to event status

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'firebase-functions';
 
 const functions = require('firebase-functions');
+const { onValueWritten } = require('firebase-functions/v2/database');
 const admin = require('firebase-admin/app');
 const { updateCurrentMatch } = require('./updateCurrentMatch');
+const { statusUpdateHandler } = require('./statusUpdateHandler');
 
 admin.initializeApp();
 functions.logger.info('Initialized Firebase app');
@@ -11,6 +13,22 @@ exports.updateCurrentMatch = functions.pubsub.schedule('every 1 minutes')
   .onRun(async () => {
     await updateCurrentMatch();
   });
+
+exports.notifyStateChanges = onValueWritten({
+  ref: '/seasons/{season}/events/{eventKey}/state',
+  instance: 'fim-queueing-default-rtdb',
+}, async (event: any) => {
+  const original = event.data.before.val();
+  const updated = event.data.after.val();
+
+  if (!original || !updated) {
+    return null;
+  }
+
+  await statusUpdateHandler(original, updated, event.params.season, event.params.eventKey);
+
+  return null;
+});
 
 exports.reportError = functions.https.onRequest(
   (req: Request, res: Response) => {
